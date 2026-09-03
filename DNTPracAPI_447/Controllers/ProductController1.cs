d08554c9c9f3eea9d0858724ef166dda7ec339e7
narkul1176 <narkul1176@gmail.com>
2026-09-03 14:56:14 +0530

    new feature show-all-products added

---
+++ DNTPracAPI_447/Controllers/ProductController1.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Abstraction;
+
+namespace DNTPracAPI_447.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController1 : ControllerBase
+    {
+        private readonly IProductRepository _productRepo;
+
+        public ProductController1(IProductRepository productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        [HttpGet]
+        [Route("ShowAllProducts")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                var products = _productRepo.GetAllProducts();
+                if (products != null)
+                {
+                    return StatusCode(StatusCodes.Status200OK, products);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex);
+            }
+            
+        }
+    }
+}
